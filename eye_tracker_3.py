fd175d1473131373018ac4dcd7ff1f48e90b9da4
Tahacrpc <tahacirpici3@gmail.com>
2025-03-20 16:45:43 +0300

    Add files via upload

---
+++ eye_tracker_3.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(rgb_frame)
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            for landmark in face_landmarks.landmark[468:478]:  
+                h, w, _ = frame.shape
+                x, y = int(landmark.x * w), int(landmark.y * h)
+                cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+
+    cv2.imshow("Göz Takibi", frame)
+    if cv2.waitKey(1)  == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
